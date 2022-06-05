ebe68595bac2effaaf5eda745e27449d9d4d67f3
grigorevanata <83876316+grigorevanata@users.noreply.github.com>
2022-06-05 15:30:57 +0300

    Dz8 auth

---
+++ resources/views/account/index.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+@section('content')
+<div id="app">
+        <div class="main menu">
+            <ul>
+                <li><a href="{{route('news')}}">Новости</a></li>
+            </ul>
+    </div>
+    <div class="offset-4">
+    <h2>Добро пожаловать, {{ Auth::user()->name }}</h2>
+    <br>
+    @if(Auth::user()->is_admin)
+        <a href="{{ route('admin.index') }}">В админку</a>
+    @endif
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\NewsController;
 
+use App\Http\Controllers\Account\IndexController as AccountController;
 use App\Http\Controllers\Admin\IndexController as AdminController;
 use App\Http\Controllers\Admin\CategoryController as AdminCategoryController;
 use App\Http\Controllers\Admin\NewsController as AdminNewsController;
@@ -19,7 +20,7 @@ use App\Http\Controllers\Admin\NewsController as AdminNewsController;
 */
 
 Route::get('/', function () {
-    return 'welcome';
+    return view('welcome');
 });
 
 Route::get('/news', [NewsController::class, 'index'])
@@ -28,12 +29,42 @@ Route::get('/news/{id}', [NewsController::class, 'show'])
 	->where('id', '\d+')
 	->name('news.show');
 
+Route::group(['middleware' => 'auth'], function(){
+	Route::get('/account', AccountController::class)
+	->name('account');
 
-//admin routes
-Route::group(['prefix' => 'admin', 'as' => 'admin.'], function() {
+
+	//admin routes
+	Route::group(['middlware' => 'admin' , 'prefix' => 'admin', 'as' => 'admin.'], function() {
     Route::get('/', AdminController::class)
     ->name('index');
     Route::resource('/categories', AdminCategoryController::class);
     Route::resource('/news', AdminNewsController::class);
-    });
+	});
+});
+
+   
+Route::get('/collection', function() {
+	$collection = collect(['Nick', 'Ben', 'Ann', 'Jil', 'Fred', 'Pit', 'July']);
+	dd($collection->map(function($item) {
+		return strtoupper($item);
+	})->filter(function($item) {
+		if(strlen($item) > 3) {
+			 return $item;
+		}
+	})->sort()->toJson());
+});
+
+Route::get('/sessions', function() {
+	session()->put('test', 'Test data');
+
+	if(session()->has('test')) {
+
+		//session()->remove('test'); удаление сессии
+	}
+	dd(session()->all());
+});
     
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
